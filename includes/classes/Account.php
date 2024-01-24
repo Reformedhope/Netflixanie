53a07432fd29044e1f2756d62c150f246fdf3ccb
reformedhope <steph_salzburg@hotmail.com>
2024-01-24 17:05:34 -0500

    added password validation

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -85,11 +85,13 @@ class Account {
     }
 
 
+   
     public function getError($error) {
         if (in_array($error, $this->errorArray)) {
-            return $error;
+            return "<span class='errorMessage'>$error</span>";
         }
     }
+    
     
 
 }
