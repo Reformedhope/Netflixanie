1282e258da4e8154837c088bb46136bf6728f55f
reformedhope <steph_salzburg@hotmail.com>
2024-02-03 16:09:04 -0500

    added functionality to remeber the information you put in the form

--- register.php
+++ register.php
@@ -25,6 +25,11 @@ if (isset($_POST["submitButton"])) {
     }
 
 }
+function getInputValue($name){
+    if(isset($_POST[$name])) {
+        echo $_POST[$name];
+    }
+}
 
 // This function allows you to strip the spaces from the beginning and end of a an imput and also lower cases all letters than uppercases the first letter. 
 function sanitizeFormString($inputText){
@@ -62,21 +67,21 @@ function sanitizeFormString($inputText){
 
 
                         <?php echo $account->getError(Constants::$firstNameCharacters); ?>
-                        <input type= "text"  name = "firstName" placeholder = "First Name" required>
+                        <input type= "text"  name = "firstName" placeholder = "First Name"  value = "<?php getInputValue("firstName"); ?>" required>
                         <!-- If you type required it will not allow you to submit the page until all boxes are filed.  -->
                         <?php echo $account->getError(Constants::$lastNameCharacters); ?>
-                        <input type= "text"  name = "lastName" placeholder = "Last Name" required>
+                        <input type= "text"  name = "lastName" placeholder = "Last Name" value = "<?php getInputValue("lastName"); ?>" required>
 
                         <?php echo $account->getError(Constants::$userNameCharacters); ?>
                         <?php echo $account->getError(Constants::$userNameTaken); ?>
-                        <input type= "text"  name = "username" placeholder = "Username" required>
+                        <input type= "text"  name = "username" placeholder = "Username"  value = "<?php getInputValue("username"); ?>"required>
 
                         <?php echo $account->getError(Constants::$emailsDontMatch); ?>
                         <?php echo $account->getError(Constants::$emailInvalid); ?>
                         <?php echo $account->getError(Constants::$emailTaken); ?>
-                        <input type= "email"  name = "email" placeholder = "Email" required>
+                        <input type= "email"  name = "email" placeholder = "Email" value = "<?php getInputValue("email"); ?>" required>
 
-                        <input type= "email"  name = "email2" placeholder = "Confirm Email" required>
+                        <input type= "email"  name = "email2" placeholder = "Confirm Email" value = "<?php getInputValue("email2"); ?>" required>
 
                         <?php echo $account->getError(Constants::$passwordDontMatch); ?>
                         <?php echo $account->getError(Constants::$passwordLength); ?>
