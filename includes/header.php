ef29fa0afb5c3a8202be3eede582ccbfbce620a6
reformedhope <steph_salzburg@hotmail.com>
2024-03-07 18:17:40 -0500

    added style to the buttons

--- includes/header.php
+++ includes/header.php
@@ -16,6 +16,7 @@ $userLoggedIn = $_SESSION["userLoggedIn"];
     <head> 
         <title> Welcome to Netflixanie</title>
         <link rel="stylesheet" type = text/css href="assets/style/style.css"/>
+        <script src="https://kit.fontawesome.com/a0c01a8968.js" crossorigin="anonymous"></script>
         
 </head>
         <body>
