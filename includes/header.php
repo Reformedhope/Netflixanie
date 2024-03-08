3b8f6af42d6580930d856a96eda97df52d111220
reformedhope <steph_salzburg@hotmail.com>
2024-03-07 19:12:39 -0500

    imported jquery

--- includes/header.php
+++ includes/header.php
@@ -16,6 +16,8 @@ $userLoggedIn = $_SESSION["userLoggedIn"];
     <head> 
         <title> Welcome to Netflixanie</title>
         <link rel="stylesheet" type = text/css href="assets/style/style.css"/>
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+        <script src="assets/js/script.js"></script>
         <script src="https://kit.fontawesome.com/a0c01a8968.js" crossorigin="anonymous"></script>
         
 </head>
