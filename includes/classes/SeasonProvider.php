3db38b35d2ebe9c299273b30d701fddc4b515583
reformedhope <steph_salzburg@hotmail.com>
2024-04-14 13:00:23 -0400

    added season number output

--- includes/classes/SeasonProvider.php
+++ includes/classes/SeasonProvider.php
@@ -1,6 +1,5 @@
 <?php
-
-class SeasonProvider{
+class SeasonProvider {
     private $con, $username;
 
     public function __construct($con, $username) {
@@ -11,16 +10,21 @@ class SeasonProvider{
     public function create($entity) {
         $seasons = $entity->getSeasons();
 
+        if(sizeof($seasons) == 0) {
+            return;
+        }
 
+        $seasonsHtml = "";
+        foreach($seasons as $season) {
+            $seasonNumber = $season->getSeasonNumber();
+
+
+            $seasonsHtml .= "<div class='season'>
+                                    <h3>Season $seasonNumber</h3>
+                                </div>";
+        }
+
+        return $seasonsHtml;
     }
-
-
-
-
 }
-
-
-
-
-
 ?>
